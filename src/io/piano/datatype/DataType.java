2716ca275920b5530b94aaaa6add1a25ff052765
Nikita Torkhov <nikita.torkhov@piano.io>
2020-04-28 23:25:41 +0400

    Second task

--- src/io/piano/datatype/DataType.java
+++ src/io/piano/datatype/DataType.java
@@ -3,6 +3,21 @@ package io.piano.datatype;
 public class DataType {
 
     public static void main(String[] args) {
-        // write your code here
+
+    byte b = Byte.parseByte("123");
+    short s = Short.parseShort("456");
+    int i = Integer.parseInt("789");
+    long l = Long.parseLong("123");
+    float f = Float.parseFloat("456.789");
+    double d = Double.parseDouble("123.456");
+
+    String string1 = Integer.toString(123);
+    String string2 = Double.toString(123.43878);
+    String string3 = Long.toString(45645646);
+    String string4 = Float.toString(909123.432f);
+
+    String string5 = Character.toString('u');
+    String string6 = Character.toString(68);
+
     }
 }
