858cad10be48318f9f1f72773aa5d7ce5f98d261
Nikita Torkhov <nikita.torkhov@piano.io>
2020-05-08 10:27:40 +0400

    Created branch. Finished strings task

--- src/io/piano/strings/Strings.java
+++ src/io/piano/strings/Strings.java
@@ -1,5 +1,20 @@
 package io.piano.strings;
 
 public class Strings {
+    public static void main(String[] args) {
+        String string1 = " Добро%пожаловать%на%курс%автоматизации ";
+        String string2 = new String("ДОБРО ПОЖАЛОВАТЬ НА КУРС АВТОМАТИЗАЦИИ ");
 
-}
+        string1 = string1.substring(1, string1.length() - 1);
+        string2 = string2.substring(0, string2.length() - 1);
+
+        string1 = string1.replaceAll("%", " ");
+
+        System.out.println("Are lines equal? " + string1.equalsIgnoreCase(string2));
+
+        System.out.println("Index of авто is " + string1.indexOf("авто"));
+        System.out.println("Char at 6th position is " + string2.charAt(6));
+
+        }
+    }
+
