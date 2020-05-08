f19b9f8b69d9767e72d4513b771e0bd43e27e6fb
Nikita Torkhov <nikita.torkhov@piano.io>
2020-05-08 11:39:17 +0400

    Finished if and switch tasks

--- src/io/piano/statements/If.java
+++ src/io/piano/statements/If.java
@@ -1,5 +1,45 @@
 package io.piano.statements;
 
-public class If {
+import java.util.Scanner;
 
+public class If {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number;
+
+        do {
+            System.out.println("Enter an integer number from 1 to 12 to display corresponding month");
+            while (!scanner.hasNextInt()) {
+                System.out.println("That's not an integer number. Try again.");
+                scanner.next();
+            }number = scanner.nextInt();
+        } while (number < 1 || number > 12 );
+
+
+        if (number == 1) {
+            System.out.println("It's January time!");
+        } else if (number == 2) {
+            System.out.println("It's February time!");
+        } else if (number == 3) {
+            System.out.println("It's March time!");
+        } else if (number == 4) {
+            System.out.println("It's April time!");
+        } else if (number == 5) {
+            System.out.println("It's May time!");
+        } else if (number == 6) {
+            System.out.println("It's June time!");
+        } else if (number == 7) {
+            System.out.println("It's July time!");
+        } else if (number == 8) {
+            System.out.println("It's August time!");
+        } else if (number == 9) {
+            System.out.println("It's September time!");
+        } else if (number == 10) {
+            System.out.println("It's October time!");
+        } else if (number == 11) {
+            System.out.println("It's November time!");
+        } else if (number == 12) {
+            System.out.println("It's December time!");
+        }
+    }
 }
--- src/io/piano/statements/Switch.java
+++ src/io/piano/statements/Switch.java
@@ -1,5 +1,58 @@
 package io.piano.statements;
 
-public class Switch {
+import java.util.Scanner;
 
+public class Switch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number;
+
+        do {
+            System.out.println
+                    ("Enter an integer number from 1 to 12 to display number of days in corresponding month");
+            while (!scanner.hasNextInt()) {
+                System.out.println("That's not an integer number. Try again.");
+                scanner.next();
+            }number = scanner.nextInt();
+        } while (number < 1 || number > 12 );
+
+        switch (number) {
+            case 1:
+                System.out.println("There are 31 days in January");
+                break;
+            case 2:
+                System.out.println("There are 28 or 29 days in February");
+                break;
+            case 3:
+                System.out.println("There are 31 days in March");
+                break;
+            case 4:
+                System.out.println("There are 30 days in April");
+                break;
+            case 5:
+                System.out.println("There are 31 days in May");
+                break;
+            case 6:
+                System.out.println("There are 30 days in June");
+                break;
+            case 7:
+                System.out.println("There are 31 days in July");
+                break;
+            case 8:
+                System.out.println("There are 31 days in August");
+                break;
+            case 9:
+                System.out.println("There are 30 days in September");
+                break;
+            case 10:
+                System.out.println("There are 31 days in October");
+                break;
+            case 11:
+                System.out.println("There are 30 days in November");
+                break;
+            case 12:
+                System.out.println("There are 31 days in December");
+                break;
+        }
+    }
 }
