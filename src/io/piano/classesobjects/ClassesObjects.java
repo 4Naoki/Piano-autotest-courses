a9687bc68221db6377a65da99b94e5573e2bcc45
Nikita Torkhov <nikita.torkhov@piano.io>
2020-05-13 18:34:54 +0400

    Created new class. Finished class object task.

--- src/io/piano/classesobjects/ClassesObjects.java
+++ src/io/piano/classesobjects/ClassesObjects.java
@@ -1,5 +1,11 @@
 package io.piano.classesobjects;
 
 public class ClassesObjects {
+    public static void main(String[] args) {
+        MyName person = new MyName();
+        person.name = "Nikita";
+        person.surname = "Torkhov";
 
+        person.sayHello(person.name, person.surname);
+    }
 }
---
+++ src/io/piano/classesobjects/MyName.java
@@ -0,0 +1,11 @@
+package io.piano.classesobjects;
+
+public class MyName {
+    String name;
+    String surname;
+
+    public void sayHello(String name, String surname) {
+        System.out.println("Greetings, " + this.name + " " + this.surname + "!");
+    }
+}
+
