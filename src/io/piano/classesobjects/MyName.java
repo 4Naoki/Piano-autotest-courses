9b111446013423a830c9261b448360338eae3c31
Nikita Torkhov <nikita.torkhov@piano.io>
2020-05-13 19:22:33 +0400

    Finished constructor task. Changed variables to public in  MyName class.

--- src/io/piano/classesobjects/MyName.java
+++ src/io/piano/classesobjects/MyName.java
@@ -1,8 +1,16 @@
 package io.piano.classesobjects;
 
 public class MyName {
-    String name;
-    String surname;
+    public String name;
+    public String surname;
+
+    public MyName(){
+
+    }
+    public MyName(String name, String surname){
+        this.name = name;
+        this.surname = surname;
+    }
 
     public void sayHello(String name, String surname) {
         System.out.println("Greetings, " + this.name + " " + this.surname + "!");
--- src/io/piano/constructors/Constructors.java
+++ src/io/piano/constructors/Constructors.java
@@ -1,5 +1,11 @@
 package io.piano.constructors;
 
+import io.piano.classesobjects.MyName;
+
 public class Constructors {
+    public static void main(String[] args) {
+        MyName person = new MyName("Nikita", "Torkhov");
+        person.sayHello(person.name, person.surname);
+    }
 
 }
