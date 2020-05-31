3631ca0eeac96bade1efdd8e56afaf35e17a176e
Nikita Torkhov <nikita.torkhov@piano.io>
2020-05-31 12:32:29 +0400

    Exception task

--- src/io/piano/exceptions/Exceptions.java
+++ src/io/piano/exceptions/Exceptions.java
@@ -1,5 +1,36 @@
 package io.piano.exceptions;
 
-public class Exceptions {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Exceptions {
+    public static void main(String[] args) {
+        Library x = new Library();
+
+
+        try {
+            x.getBook("Sunset");
+        } catch (LibException e) {
+
+        }
+    }
+
+    public static class Library {
+        private Map<String, String> books = new HashMap<>();
+
+
+        public String getBook(String title) throws LibException {
+            String s = books.get(title);
+            if (s == null) {
+                throw new LibException("no book");
+            }
+            return s;
+        }
+    }
+
+    public static class LibException extends Exception {
+        public LibException(String reason) {
+            super(reason);
+        }
+    }
 }
